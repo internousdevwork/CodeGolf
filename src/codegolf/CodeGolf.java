25ebf5bb36e541c5b8a6c1feab97d7b635a194f8
k-kawamura-internous <k.kawamura@internous.co.jp>
2021-01-26 13:09:45 +0900

    コメントの表記を統一、コメントの位置を変更

--- src/codegolf/CodeGolf.java
+++ src/codegolf/CodeGolf.java
@@ -1,12 +1,10 @@
 package codegolf;
-
+/*
+ * コードの内容
+ * 1～100の数字をコンソールに出力するソースコードを作成してください。
+ * ただし3の倍数はFizzを5の倍数はBuzzを表示してください。
+ */
 public class CodeGolf {
-
-	/*
-	 * コードの内容
-	 * 1～100の数字をコンソールに出力するソースコードを作成して下さい。
-	 * ただし3の倍数はFizzを5の倍数はBuzzを表示してください
-	 */
 	public static void main(String[] args) {
 		for (int i=1; i<=100; i++) {
 			if(i % 3 == 0 && i % 5 == 0){
